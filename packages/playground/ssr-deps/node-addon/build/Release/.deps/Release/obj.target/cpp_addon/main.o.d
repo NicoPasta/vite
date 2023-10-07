fa7d4832fd9ddb9adbd0fe2bbb53f27439358093
张玉鑫 <zhangyuxin9102@163.com>
2023-10-07 21:42:15 +0800

    1

---
+++ packages/playground/ssr-deps/node-addon/build/Release/.deps/Release/obj.target/cpp_addon/main.o.d
@@ -0,0 +1,19 @@
+cmd_Release/obj.target/cpp_addon/main.o := c++ -o Release/obj.target/cpp_addon/main.o ../main.cpp '-DNODE_GYP_MODULE_NAME=cpp_addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node -I/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/src -I/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/deps/openssl/config -I/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/deps/openssl/openssl/include -I/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/deps/uv/include -I/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/deps/zlib -I/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/deps/v8/include  -O3 -gdwarf-2 -mmacosx-version-min=10.13 -arch arm64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++14 -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/cpp_addon/main.o.d.raw   -c
+Release/obj.target/cpp_addon/main.o: ../main.cpp \
+  /Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/node.h \
+  /Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/v8.h \
+  /Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/cppgc/common.h \
+  /Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/v8config.h \
+  /Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/v8-internal.h \
+  /Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/v8-version.h \
+  /Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/v8-platform.h \
+  /Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/node_version.h
+../main.cpp:
+/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/node.h:
+/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/v8.h:
+/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/cppgc/common.h:
+/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/v8config.h:
+/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/v8-internal.h:
+/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/v8-version.h:
+/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/v8-platform.h:
+/Users/zhangyuxin/Library/Caches/node-gyp/16.20.2/include/node/node_version.h:
